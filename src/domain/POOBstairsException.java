b0c9493f332e1fcd51d5f23396f9e8f4ae5ed66b
Parralol <110953563+Parralol@users.noreply.github.com>
2023-04-26 14:40:56 -0500

    nueva excepcion

--- src/domain/POOBstairsException.java
+++ src/domain/POOBstairsException.java
@@ -11,6 +11,7 @@ public class POOBstairsException extends Exception {
     static String NO_PUEDE_TENER_MISMO_TURNO = "Error en los turnos";
     static String NO_PUEDE_TENER_MISMO_COLOR = "Error en los colores, no pueden existir dos iguales";
     static String JUGADOR_DEBE_TENER_NOMBRE = "El jugador debe de poseer nombre";
+    static String dEBE_EXISTIR_2JUGADORES = "No puedes colocar 0 jugadores";
 
     public POOBstairsException(String message) {
         super(message);
