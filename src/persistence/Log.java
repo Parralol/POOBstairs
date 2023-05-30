c3764b3c5ae71aea75cb9a0ea523e1085d6e83ba
Parralol <110953563+Parralol@users.noreply.github.com>
2023-05-29 21:50:45 -0500

    logger

---
+++ src/persistence/Log.java
@@ -0,0 +1,10 @@
+package persistence;
+
+import java.util.logging.LogManager;
+
+import org.apache.logging.*;
+import org.apache.logging.log4j.Logger;
+
+public class Log {
+    //private static final Logger LOOGER;
+}
